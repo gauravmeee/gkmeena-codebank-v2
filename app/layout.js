2058fb08dfeb6a8db1853e9c2403239b9712da6e
Gaurav <gaurav28.official@gmail.com>
2025-02-16 14:27:13 +0530

    google analytics added

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";  // Import Footer component
 import { ThemeProvider } from "@/components/theme-provider"
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +15,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-p-20 scroll-smooth">
+
       <body className={`${inter.className} flex flex-col min-h-screen`}>
+
+      { /*Add Google Analytics */}
+        <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-7FLDG657GW"/>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-7FLDG657GW');
+          `}
+        </Script>
+
       <ThemeProvider
             attribute="class"
             defaultTheme="system"
