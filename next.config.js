a1c5fd78cee8384020cb6aca5dcad1a5d92213e0
Gaurav <gaurav28.official@gmail.com>
2025-04-15 23:52:53 +0530

    problem solved, working fine now

--- next.config.js
+++ next.config.js
@@ -2,23 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com', 'firebasestorage.googleapis.com'],
+    domains: ['images.unsplash.com', 'firebasestorage.googleapis.com', 'logo.clearbit.com'],
   },
-  // Disable static page generation for problematic routes
-  // This will make them render on-demand instead
-  experimental: {
-    // Increase the static page generation timeout
-    staticPageGenerationTimeout: 120,
-  },
-  // Configure which pages should be statically generated
-  // and which should be server-rendered
-  async rewrites() {
-    return [
-      // Add any API rewrites here if needed
-    ];
-  },
-  // Configure which pages should be statically generated
-  // and which should be server-rendered
   async headers() {
     return [
       {
@@ -34,4 +19,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig; 
+module.exports = nextConfig;
