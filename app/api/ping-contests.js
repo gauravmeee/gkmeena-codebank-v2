51fcc0877f85d5dc54e58047258a201ff8dbac67
Gaurav <gaurav28.official@gmail.com>
2025-08-01 01:57:43 +0530

    fix: convert API routes (api/ping-...) to Next.js App Router (ping-.../route.js) format for proper redirects

--- app/api/ping-contests.js
+++
@@ -1,3 +0,0 @@
-export default function handler(req, res) {
-  res.redirect("https://flask-contest-api.onrender.com");
-}
--- app/api/ping-jobs.js
+++
@@ -1,3 +0,0 @@
-export default function handler(req, res) {
-    res.redirect("https://flask-jobs-api.onrender.com/");
-  }
